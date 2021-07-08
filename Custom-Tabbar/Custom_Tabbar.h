c262b4a2cd02ff4183dd72a6d142840a6afe938a
user195713 <user195713@telehouse-moboware.dmarc.si1.atlanticmetro.net>
2021-07-08 12:40:40 -0400

    finalizando layout pronto para teste

---
+++ Custom-Tabbar/Custom_Tabbar.h
@@ -0,0 +1,19 @@
+//
+//  Custom_Tabbar.h
+//  Custom-Tabbar
+//
+//  Created by Mauro Figueiredo on 07/07/21.
+//  Copyright © 2021 Mauro Figueiredo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Custom_Tabbar.
+FOUNDATION_EXPORT double Custom_TabbarVersionNumber;
+
+//! Project version string for Custom_Tabbar.
+FOUNDATION_EXPORT const unsigned char Custom_TabbarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Custom_Tabbar/PublicHeader.h>
+
+
